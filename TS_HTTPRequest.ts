ea468a7844d41d4588d66b974aab64ef56872921
dewiap <38904408+dewiap@users.noreply.github.com>
2022-11-28 22:30:05 +0700

    Add files via upload

---
+++ TS_HTTPRequest.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>TS_HTTPRequest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>88aefe23-5b2b-40e4-8ab7-f9dab9e3fc71</testSuiteGuid>
+   <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
+   <filteringExtension></filteringExtension>
+   <filteringPlugin></filteringPlugin>
+   <filteringText>id=(Test Cases) </filteringText>
+</FilteringTestSuiteEntity>
